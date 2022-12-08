4741b1ed49875fec7896b8a3a1f21ef94e3cf197
sangitasarkar74 <sangitasarkar@gmail.com>
2022-12-09 00:21:09 +0530

    First stage completed

--- src/components/App.js
+++ src/components/App.js
@@ -2,15 +2,14 @@ import React from 'react'
 import '../styles/App.css';
 const App = () => {
 
-  const handleDoubleClick = (event) =>{
-  
-  }
+
   return (
     <div id="main">
-      <button id="dblclick-btn">Double click me</button>
+      <button id="dblclick-btn" onDoubleClick={handleDoubleClick}>Double click me</button>
+      {handleDoubleClick ? console.log("I was double clicked") : console.log("I was not double clicked")}
     </div>
   )
-}
 
+}
 
 export default App;
