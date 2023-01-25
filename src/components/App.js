9ed20542d7a60ac6b10e64dcc1612851768201db
sangitasarkar74 <sangitasarkar@gmail.com>
2023-01-25 20:05:42 +0530

    More changes done

--- src/components/App.js
+++ src/components/App.js
@@ -1,21 +1,27 @@
-import React from 'react'
-import '../styles/App.css';
+import "../styles/App.css";
 const App = () => {
-  const addEventHandler = true;
+  // const addEventHandler = true;
 
   const handleDoubleClick = (event) => {
-    console.log("I was double clicked");
-  }
-  const noDoubleClick = (event) => {
-    console.log("I was not double clicked");
-  }
+    // console.log("I was double clicked");
+    if (event.detail == 2) {
+      console.log("I was double clicked");
+    } else {
+      console.log("I was not double clicked");
+    }
+  };
+  // const noDoubleClick = (event) => {
+  //   console.log("I was not double clicked");
+  //   console.log(event);
+  //   setAddEventHandler("I was not double clicked");
+  // };
   return (
     <div id="main">
-      <button id="dblclick-btn" onDoubleClick={addEventHandler ? handleDoubleClick : noDoubleClick}>Double click me</button>
-
+      <button id="dblclick-btn" onClick={handleDoubleClick}>
+        Double click me
+      </button>
     </div>
-  )
-}
-
+  );
+};
 
 export default App;
